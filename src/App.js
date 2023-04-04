3daa3855b418b694f5c9a9c314bb10b5b523abfa
Lydsm4 <admyaile@gmail.com>
2023-04-04 00:45:04 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Video from "./Pages/Video"
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app_videos'>
+      <Video /> 
+      <Video />
+      <Video />
+      <Video />
+      </div>
+    
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Video.js
@@ -0,0 +1,36 @@
+import React, {useRef, useState} from 'react'
+import "./video.css"
+
+function Video() {
+    
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+    function handdleStart(){
+          
+        if(play){
+            videoRef.current.pause()
+            setPlay(false)
+        }
+        else{
+            videoRef.current.play()
+            setPlay(true)
+        }
+    }
+
+  return (
+    <div className='video'> 
+         
+        <video 
+            className='videoplay'
+            ref={videoRef}
+            onClick={handdleStart}
+            loop
+            src='https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/brecker2.mp4?alt=media&token=b7e3ebf7-89a0-47a0-8a34-66f1176a71e3'
+        >
+        </video>
+        
+    </div>
+  )
+}
+
+export default Video
